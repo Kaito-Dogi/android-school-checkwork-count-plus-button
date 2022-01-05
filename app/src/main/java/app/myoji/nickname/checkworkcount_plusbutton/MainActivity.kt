1f4e888807f2f1d47bc187b6e50c1a061f778463
Kaito-Dogi <kendobu0405@gmail.com>
2022-01-05 19:19:48 +0900

    add: View Bindingの設定

--- app/src/main/java/app/myoji/nickname/checkworkcount_plusbutton/MainActivity.kt
+++ app/src/main/java/app/myoji/nickname/checkworkcount_plusbutton/MainActivity.kt
@@ -2,10 +2,15 @@ package app.myoji.nickname.checkworkcount_plusbutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.myoji.nickname.checkworkcount_plusbutton.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // バインディングクラスの変数
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
     }
 }
